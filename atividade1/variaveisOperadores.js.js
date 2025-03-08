587b700790d38a81c08a612a53d26ba78332e282
Deborah-Araujo <135188849+Deborah-Araujo@users.noreply.github.com>
2025-03-07 21:01:01 -0300

    Add - variaveisOperadores

---
+++ atividade1/variaveisOperadores.js.js
@@ -0,0 +1,15 @@
+//Atividade aula 2 --> Programação pra Web
+let valor1 = 10;
+let valor2 = 5;
+
+let soma = valor1 + valor2;
+console.log(`O resultado da soma entre ${valor1} e ${valor2} é:\n ${soma}`);
+
+let subtracao = valor1 - valor2;
+console.log(`O resultado da subtracao entre ${valor1} e ${valor2} é:\n ${subtracao}`);
+
+let multiplicacao = valor1 * valor2;
+console.log(`O resultado da multiplicação entre ${valor1} e ${valor2} é:\n ${multiplicacao}`);
+
+let divisao = valor1 / valor2;
+console.log(`O resultado da divisão entre ${valor1} e ${valor2} é:\n ${divisao}`);
