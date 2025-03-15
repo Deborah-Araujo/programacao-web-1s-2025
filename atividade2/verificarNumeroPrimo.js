8b0d746fd4e6b21e5b32caf1f372b20b4753c1e7
Deborah-Araujo <deby.limara@gmail.com>
2025-03-14 22:32:57 -0300

    add verificarNumeroPrimo

---
+++ atividade2/verificarNumeroPrimo.js
@@ -0,0 +1,27 @@
+function verificarNumeroPrimo(n){
+    if(n <= 1){
+        return false;
+    }
+
+    if(n % 2 == 0 && n != 2){
+        return false;
+    }
+
+    for(let i = 3; i < n; i += 2){
+        if(n % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(`verificarNumeroPrimo(0)=${verificarNumeroPrimo(0)}`); //false
+console.log(`verificarNumeroPrimo(0)=${verificarNumeroPrimo(1)}`); //false
+console.log(`verificarNumeroPrimo(0)=${verificarNumeroPrimo(2)}`); //true
+console.log(`verificarNumeroPrimo(0)=${verificarNumeroPrimo(3)}`); //true
+console.log(`verificarNumeroPrimo(0)=${verificarNumeroPrimo(7)}`); //true
+console.log(`verificarNumeroPrimo(0)=${verificarNumeroPrimo(83)}`); //true
+console.log(`verificarNumeroPrimo(0)=${verificarNumeroPrimo(100)}`); //false
+console.log(`verificarNumeroPrimo(0)=${verificarNumeroPrimo(991)}`); //true
+console.log(`verificarNumeroPrimo(0)=${verificarNumeroPrimo(104729)}`); //true
+console.log(`verificarNumeroPrimo(0)=${verificarNumeroPrimo(14348907)}`); //false
